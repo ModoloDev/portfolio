caa9196f71733ed00153a75d2f1f9935c3d7459c
Lucas Módolo <lucasmg2001@gmail.com>
2024-06-13 14:56:38 -0400

    fix: certificates page scroll and other minors adjustments

--- src/components/Screen/Pages/AboutMe/ABOUT_ME_PAGE.tsx
+++ src/components/Screen/Pages/AboutMe/ABOUT_ME_PAGE.tsx
@@ -16,7 +16,7 @@ export const ABOUT_ME_PAGE: PAGE = {
         `Hey there! Welcome to my Portfolio!
         
         
-        My name is Lucas, I'm a Software Engineer with over ${new Date().getFullYear() - 2021} years of experience in backend development. I love working with JavaCore and frameworks like Spring Boot and Quarkus. I'm also pretty savvy with cloud tech, using AWS and Azure. 
+        My name is Lucas, I'm a Software Engineer with over ${new Date().getFullYear() - 2021} years of experience in backend development. I love working with Core Java and frameworks like Spring Boot and Quarkus. I'm also pretty savvy with cloud tech, using AWS and Azure. 
         
         I'm all about design patterns, SOLID principles, Clean Architecture, and Domain-Driven Design to keep my code high-quality, scalable, and easy to maintain. My tech skills and strategic thinking in cloud infrastructure have really helped boost system performance and reliability.
         
--- src/components/Screen/Pages/Certificates/CERTIFICATES_PAGE.tsx
+++ src/components/Screen/Pages/Certificates/CERTIFICATES_PAGE.tsx
@@ -7,9 +7,11 @@ import './Certificates.css'
 export const CERTIFICATES_PAGE: PAGE = {
   title: 'Certificates',
   content: <section className='certificates'>
-    <a className="certificate" href="https://www.credly.com/badges/64de666a-5e2d-4278-9143-57e2bcfc940e/public_url" target="_blank">
-      <img className="certificateImage" src={awsCertifiedCloudPractitioner}/>
-      <p className="certificateTitle">ARQUITETURA CSS: DESCOMPLICANDO OS PROBLEMAS</p>
+    <a className="certificate" href="https://www.credly.com/badges/64de666a-5e2d-4278-9143-57e2bcfc940e/public_url"
+       target="_blank">
+      <img className="certificateImage" src={awsCertifiedCloudPractitioner}
+           alt="AWS Certified Cloud Practitioner Badge"/>
+      <p className="certificateTitle">AWS Certified Cloud Practitioner</p>
     </a>
   </section>
 }
